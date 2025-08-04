0c360282f5e4c958e4c53c2910d4b7f6372ebd1e
pyscalp <pyscalp@gmail.com>
2025-08-04 13:47:17 +0700

    Add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+})
